3903da576bb5b5eb719e0d1a8817e8b31565b700
Kalyn <kalyn.dorheim@pnnl.gov>
2020-02-05 11:47:38 -0500

    Add fixes to the variable information table and unit tests.

--- tests/testthat/test.ed-variable-info.R
+++ tests/testthat/test.ed-variable-info.R
@@ -5,4 +5,10 @@ test_that("ED2 variable info works", {
     ed2_variable_info("My fake var"),
     "variables not found in ED2 variables table: My fake var"
   )
-})
+  out <- ed2_variable_info()
+  df_results <- c(out$df_cohort, out$df_pft, out$df_scalar, out$df_soil)
+  # Make sure that all of the df indicators are T/F and that there are no NAs. 
+  expect_equal(sum(is.na(df_results)), 0)
+  expect_true(is.logical(df_results))
+  })
+
